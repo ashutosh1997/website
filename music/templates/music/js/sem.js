e68eb016c9482eb9b7d80136e6f2b9c1cdb6bab4
ASHUTOSH <kumar.ashutosh920@gmail.com>
2017-01-10 01:22:56 +0530

    Done

---
+++ music/templates/music/js/sem.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+    $(".table").hide();
+});
+
+$(document).ready(function(){
+
+    $("#l1 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t1").show();
+    });
+
+    $("#l2 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t2").show();
+    });
+
+    $("#l3 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t3").show();
+    });
+
+    $("#l4 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t4").show();
+    });
+
+    $("#l5 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t5").show();
+    });
+
+    $("#l6 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t6").show();
+    });
+
+    $("#l7 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t7").show();
+    });
+
+    $("#l8 a").click(function(){
+        $("#default_image").hide();
+        $(".table").hide();
+        $("#t8").show();
+    });
+
+});
